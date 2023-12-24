d606afe903de92f931c48bc0c0b7ab000ce53ddb
Kareem-Bizreh <kareembezreh@gmail.com>
2023-12-24 13:52:11 +0300

    kareem

--- src/Log.java
+++ src/Log.java
@@ -1,2 +1,77 @@
-package PACKAGE_NAME;public class Log {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Log extends JFrame{
+
+    Log(){
+        setIconImage(new ImageIcon("cinema/test.png").getImage());
+        setResizable(false);
+        setSize(700,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel usernameLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField(20);
+        Font font = usernameField.getFont();
+        font = font.deriveFont(font.getSize() + 25f);
+        usernameField.setFont(font);
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField(20);
+        passwordField.setFont(font);
+        JButton loginButton = new JButton("Login");
+        loginButton.setFocusable(false);
+        JButton logupButton = new JButton("Logup");
+        logupButton.setFocusable(false);
+        JButton Exit = new JButton("Exit");
+        Exit.setFocusable(false);
+        JButton Back = new JButton("Back");
+        Back.setFocusable(false);
+        JPanel panel = new JPanel(new GridLayout(4, 2));
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(loginButton);
+        panel.add(logupButton);
+        panel.add(Back);
+        panel.add(Exit);
+        panel.setSize(500,500);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = passwordField.getText();
+                if(username.isEmpty() || password.isEmpty()){
+                    JOptionPane.showMessageDialog(null,
+                            "Information is incomplete",
+                            "Error",JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                //Logic
+            }
+        });
+        logupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new LogUp();
+            }
+        });
+        Exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        Back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new Welcome();
+            }
+        });
+        add(panel);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
--- src/LogUp.java
+++ src/LogUp.java
@@ -1,2 +1,77 @@
-package PACKAGE_NAME;public class LogUp {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LogUp extends JFrame {
+    LogUp(){
+        setIconImage(new ImageIcon("cinema/test.png").getImage());
+        setResizable(false);
+        setSize(700,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel usernameLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField(20);
+        Font font = usernameField.getFont();
+        font = font.deriveFont(font.getSize() + 25f);
+        usernameField.setFont(font);
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField(20);
+        JLabel Confirm = new JLabel("Confirm Password:");
+        JPasswordField passwordConfirm = new JPasswordField();
+        passwordField.setFont(font);
+        passwordConfirm.setFont(font);
+        JButton loginButton = new JButton("Login");
+        loginButton.setFocusable(false);
+        JButton Exit = new JButton("Exit");
+        Exit.setFocusable(false);
+        JButton Back = new JButton("Back");
+        Back.setFocusable(false);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = passwordField.getText();
+                String confirmPassword = passwordConfirm.getText();
+                if(!password.equals(confirmPassword)){
+                    JOptionPane.showMessageDialog(null,"Password does not match confirmation",
+                            "Error",JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                if(username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()){
+                    JOptionPane.showMessageDialog(null,
+                            "Information is incomplete",
+                            "Error",JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                //Logic
+            }
+        });
+        Exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        Back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new Log();
+            }
+        });
+        JPanel panel = new JPanel(new GridLayout(5, 2));
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(Confirm);
+        panel.add(passwordConfirm);
+        panel.add(loginButton);
+        panel.add(Back);
+        panel.add(Exit);
+        panel.setSize(500,500);
+        add(panel);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
